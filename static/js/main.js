ff3c5152963fd732faa4804791e6b9b397b0297a
Tô Trường Chinh <114303593+ChinhToTruong@users.noreply.github.com>
2023-03-04 22:28:23 +0700

    first commit

---
+++ static/js/main.js
@@ -0,0 +1,22 @@
+// popover
+$(function () {
+  $('[data-toggle="popover"]').popover();
+});
+
+// Gender select
+// if (window.location.pathname === '/') {
+//   const radioBtn1 = document.querySelector("#exampleRadios1");
+//   const radioBtn2 = document.querySelector("#exampleRadios2");
+//   const radioBtn3 = document.querySelector("#exampleRadios3");
+//   const genderSelect = document.querySelector("#genderSelect");
+
+//   radioBtn1.addEventListener("change", () => {
+//     genderSelect.classList.add('d-none');
+//   });
+//   radioBtn2.addEventListener("change", () => {
+//     genderSelect.classList.add('d-none');
+//   });
+//   radioBtn3.addEventListener("change", () => {
+//     genderSelect.classList.remove('d-none');
+//   });
+// }
